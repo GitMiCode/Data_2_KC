1344826b959f111c7f9d53bf2f8312ee9cc377d2
gitmicode <gitmicode@jpakz.com>
2022-05-24 19:33:50 -0400

    first commit, first knowledge check

---
+++ knowledge_check_1 .py
@@ -0,0 +1,30 @@
+print ('hello world!')
+
+greetings = ['hi', 'hello', 'hiya', 'hola earthlings' ]
+print(greetings[3])
+
+
+foodfav = {
+    'mexican' : 'Carnitas',
+    'chinese' : 'Fried rice',
+    'greek' : 'Gyro'
+}
+print(foodfav['mexican'])
+
+
+earthlings = [
+    {
+        "height in ft" : 5,
+        "weight" : 220,
+        "state" : "KY",
+        "current loc": (38.2259, 85.7686),
+        "weather" : "Summer" },
+    {
+        "height in ft" : 3,
+        "weight" : 80,
+        "state" : "NY",
+        "current loc": (38.2259, 85.7686),
+        "weather" : "Fall" }
+]
+
+print("current locations" + earthlings[1]["current loc"])
