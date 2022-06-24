44d43876cea951d932c6b53aa29a3a476ee95d2d
GitMiCode <102746154+GitMiCode@users.noreply.github.com>
2022-06-24 11:26:05 -0400

    Update kc_2 path for csv

--- kc_2.py
+++ kc_2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 
 #data = pd.read_csv("40165.csv")
-df = pd.read_csv("D:\\_MyPython\\Indices_of_farm_output.csv")
+df = pd.read_csv("assets/Indices_of_farm_output.csv")
 
 total_Agri_Out = df[(df["Attribute"] == "Total agricultural output")]
 #print(df)
